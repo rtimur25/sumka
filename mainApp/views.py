a0d88eeee24b7ed0844a5659456487bd30608801
Timur Rylov <rtimur1999@gmail.com>
2018-02-03 23:25:57 +0300

    First commit

---
+++ mainApp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views ehere.
+def index(request):
+    return render(request, 'mainApp/homePage.html')
+
+
+def contact(request):
+    return render(request, 'mainApp/basic.html' , {'values' : [ 'text you' ,'234']})
